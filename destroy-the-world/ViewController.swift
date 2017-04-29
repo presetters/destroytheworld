27ee292ea46b7669103e44b08564d4a683924f58
Demchenko Peter <presetter@Demchenkos-MacBook-Pro.local>
2017-04-29 17:31:29 +0300

    First commit to GitHub

---
+++ destroy-the-world/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  destroy-the-world
+//
+//  Created by Demchenko Peter on 29/04/2017.
+//  Copyright © 2017 Cubic Media. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print("Hello World!")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
